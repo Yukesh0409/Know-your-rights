710d369da612bdee26c58f2bc62be41bfd43ff91
Yukesh0409 <Yukesh0409>
2023-09-24 13:29:15 +0530

    adding legalrights

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for, request, jsonify
+import json
+from google.oauth2 import service_account
+import gspread
+import pandas as pd
 
 app = Flask("__name__")
+
+credentials = service_account.Credentials.from_service_account_file(
+    "static/data/indian-legal-information-d6444bb36676.json",
+    scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
+)
+
+spreadsheet_id = '1KHrJxFzRNfCFM3POGqdXvTjXmoZG9Cx79vNeznaFPQI'
+
+client = gspread.authorize(credentials)
+sheet = client.open_by_key(spreadsheet_id).sheet1
+
+
 
 @app.route("/")
 def home():
@@ -9,10 +25,6 @@ def home():
 @app.route("/readdressal")
 def readdressal():
     return render_template("readdressal.html")
-
-@app.route("/legal-rights")
-def legalrights():
-    return render_template("legalrights.html")
 
 @app.route("/law-and-regulations")
 def Lawandregulations():
@@ -30,6 +42,65 @@ def events():
 def documents():
     return render_template("Documents.html")
 
+def convert_sheet_to_json(df):
+    json_data = []
+    current_rights = None
+    current_articles = []
+    current_desc = []
+
+    for index,row in df.iterrows():
+        if not pd.isnull(row['RIGHTS']):
+            if current_rights is not None:
+                json_data.append({'RIGHTS': current_rights, 'LIST_OF_ARTICLES': current_articles, 'DESCRIPTION': current_desc})
+            current_rights = row['RIGHTS']
+            current_articles = []
+            current_desc = []
+        if not pd.isnull(row['LIST_OF_ARTICLES']):
+            current_articles.append(row['LIST_OF_ARTICLES'])
+        if not pd.isnull(row['DESCRIPTION']):
+            current_desc.append(row['DESCRIPTION'])
+
+    if current_rights is not None:
+        json_data.append({'RIGHTS': current_rights, 'LIST_OF_ARTICLES': current_articles, 'DESCRIPTION': current_desc})
+
+    json_string = json.dumps(json_data, indent=4)
+
+    return json_string
+
+df = pd.DataFrame(sheet.get_all_records())
+data_json = convert_sheet_to_json(df)
+
+data = json.loads(data_json)
+
+def group_data_by_right(data):
+    right_data = {}
+    for entry in data:
+        right = entry["RIGHTS"]
+        if right not in right_data:
+            right_data[right] = {"LIST_OF_ARTICLES": [], "DESCRIPTION": []}
+        right_data[right]["LIST_OF_ARTICLES"].extend(entry["LIST_OF_ARTICLES"])
+        right_data[right]["DESCRIPTION"].extend(entry["DESCRIPTION"])
+    return right_data
+
+grouped_data = group_data_by_right(data)
+
+@app.route('/legal-rights', methods=['GET'])
+def legalrights():
+    return render_template('legalrights.html', grouped_data=grouped_data)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    results = []
+
+    if not query:
+        return jsonify(data)
+
+    for entry in data:
+        if entry["RIGHTS"] == query:
+            results.append(entry)
+
+    return jsonify(results)
 
 
 
